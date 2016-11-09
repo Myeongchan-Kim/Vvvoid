79ffbcbf51be37e0045da1d9389d6d83eeff63ee
Taewoo Kim <kimsin3003@naver.com>
2016-11-09 23:24:05 +0900

    Background moving complete.

---
+++ Vvvoid/Assets/StatManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public float GetSpeed() { return 4.0f; }
+
+}
