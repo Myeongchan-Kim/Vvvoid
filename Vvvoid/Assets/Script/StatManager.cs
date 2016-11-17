f9cba38277f596c0416e5fa447737081df287aac
Paul MC Kim <kmc5500@naver.com>
2016-11-17 23:55:22 +0900

    Accel by Click

--- Vvvoid/Assets/Script/StatManager.cs
+++ Vvvoid/Assets/Script/StatManager.cs
@@ -6,45 +6,90 @@ using System;
 public class StatManager : MonoBehaviour {
 
     public const double DEFALT_SPEED = 1.0;
-    public const double DEFALT_MASS = 100;
-    public double currentScale = 1.0;
+    public const double DEFALT_MASS = 10;
+    public double currentScaleStep = 0.0;
     double _velocity;
     double _distance;
     double _fuelAmout;
-    double _maxFuelAmout;
+    public double _maxFuelAmout;
     double _mass;
+    double _fuelConsumtionForEachTouch = 1.0;
 
-    Text distanceUI = null;
-    Text velocityUI = null;
+    Text _distanceUI = null;
+    Text _velocityUI = null;
+    Text _fuelUI = null;
 
     void Start () {
         _velocity = DEFALT_SPEED;
         _distance = 0.0;
+        _fuelAmout = _maxFuelAmout * 0.5;
+        _mass = DEFALT_MASS;
 
-        distanceUI = GameObject.Find("DistanceText").GetComponent<Text>();
-        velocityUI = GameObject.Find("VelocityText").GetComponent<Text>();    
+        _distanceUI = GameObject.Find("DistanceText").GetComponent<Text>();
+        _velocityUI = GameObject.Find("VelocityText").GetComponent<Text>();
+        _fuelUI = GameObject.Find("FuelText").GetComponent<Text>();
 	}
 	
 	void Update () {
         _distance += (double)Time.deltaTime * _velocity;
-        distanceUI.text = String.Format("Dist : {0:N3} m", _distance);
-        velocityUI.text = String.Format("Speed : {0:N3} m/s", _velocity);
+        _distanceUI.text = String.Format("Dist : {0:N3} m", _distance);
+        _velocityUI.text = String.Format("Speed : {0:N3} m/s", _velocity);
+        _fuelUI.text = String.Format("Fuel : {0:N1} / {1:N1} ", _fuelAmout, _maxFuelAmout);
         
+        if(Input.GetButtonDown("Fire1"))
+        {
+            double consumedEnergy = AccelCharacter(_fuelConsumtionForEachTouch);
+            if (consumedEnergy > 0.0)
+            {
+                AnimateAccel(consumedEnergy);
+            }
+        }
     }
 
-    public float GetScrollSpeed() { return (float)(_velocity / Math.Pow(2.0, currentScale)); }
+    IEnumerator AnimateAccel(double consumeAmount)
+    {
+        yield return null;
+    }
+
+    public float GetScrollSpeed() { return (float)(_velocity / Math.Pow(2.0, currentScaleStep)); }
 
     //clicker game has to have stat by double
     public double GetRealSpeed() { return _velocity; }
 
     public double AccelCharacter(double energy)
     {
-        double cur_energy = 0.5 * _velocity * _velocity;
-        cur_energy += energy;
-        _velocity = Math.Sqrt(cur_energy * 2);
+        if (_fuelAmout - energy > 0)
+        {
+            _fuelAmout -= energy;
+        }
+        else
+        {
+            _fuelAmout = 0;
+            energy = _fuelAmout;
+        }
 
-        //it return result Velocity
-        return GetRealSpeed();
+        _velocity = AddSpeed(_velocity, _mass, energy);
+        //it return consumed energy
+        return energy;
     }
 
+    double AddSpeed(double curVelocity , double targetMass, double energyToAdd)
+    {
+        double cur_energy = 0.5 * curVelocity * curVelocity * targetMass;
+        cur_energy += energyToAdd;
+        curVelocity = Math.Sqrt(cur_energy * 2 / targetMass);
+
+        return curVelocity;
+    }
+
+    public double GetFuel(double getAmout)
+    {
+        if (getAmout + _fuelAmout < _maxFuelAmout)
+            _fuelAmout += getAmout;
+        else
+            _fuelAmout = _maxFuelAmout;
+
+        return _fuelAmout;
+    }
+    
 }
