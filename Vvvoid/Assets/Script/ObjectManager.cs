5227ccdb6ccd7acd526b339d4dc062eb3020253f
FTT\kimsin <kimsin3003@naver.com>
2016-11-21 21:03:51 +0900

    now object manager inactivates meteors outside of cullingbox !!

--- Vvvoid/Assets/Script/ObjectManager.cs
+++ Vvvoid/Assets/Script/ObjectManager.cs
@@ -6,24 +6,38 @@ public class ObjectManager : MonoBehaviour {
     public GameObject meteorPrefab = null;
     public float startXPosition;
     public float yPositionMax;
-    private List<GameObject> meteors;
-    private float elapsedTime = 0;
     public StatManager statManager;
+    public BoxCollider cullingBox;
+
+    private List<GameObject> resourcePool;
+    private float elapsedTime = 0;
+    private Queue<int> freeObjectIndex;
 
 	// Use this for initialization
 	void Start () {
-        meteors = new List<GameObject>();
+        resourcePool = new List<GameObject>();
+        freeObjectIndex = new Queue<int>();
+        for(int i = 0; i < 30; i++)
+        {
+            GameObject obj = Instantiate<GameObject>(meteorPrefab);
+            obj.SetActive(false);
+            resourcePool.Add(obj);
+            freeObjectIndex.Enqueue(i);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         elapsedTime += Time.deltaTime;
         if (elapsedTime > 1)
         {
             elapsedTime = 0;
-            GameObject meteor = Instantiate<GameObject>(meteorPrefab);
+            int newObjectIndex = freeObjectIndex.Dequeue();
+            GameObject meteor = resourcePool[newObjectIndex];
+            meteor.SetActive(true);
             meteor.transform.position = new Vector3(startXPosition, Random.Range(-yPositionMax, yPositionMax), 0);
-            meteors.Add(meteor);
+            resourcePool.Add(meteor);
         }
         
         if (Input.GetMouseButtonDown(0))
@@ -35,20 +49,29 @@ public class ObjectManager : MonoBehaviour {
             {
                 if (hit.transform.tag == "Resource")
                 {
-                    Resource resource = hit.transform.gameObject.GetComponent<Resource>();
-                    statManager.AddResource(resource.containingResource);
                     GameObject obj = hit.transform.gameObject;
+                    if (obj.activeSelf)
+                    {
+                        Debug.Log("Hit");
+                        Resource resource = obj.GetComponent<Resource>();
+                        statManager.AddResource(resource.containingResource);
 
-                    obj.SetActive(false);
-                    meteors.Remove(obj);
-                    Destroy(obj);
+                        obj.SetActive(false);
+                        freeObjectIndex.Enqueue(resourcePool.IndexOf(obj));
+                    }
                 }
             }
         }
 
-        foreach (var meteor in meteors)
+        foreach (var meteor in resourcePool)
         {
+            if(!cullingBox.bounds.Contains(meteor.transform.position))
+            {
+                meteor.SetActive(false);
+                freeObjectIndex.Enqueue(resourcePool.IndexOf(meteor));
+            }
             meteor.transform.position -= new Vector3(0.1f, 0, 0);
         }
-	}
+
+    }
 }
